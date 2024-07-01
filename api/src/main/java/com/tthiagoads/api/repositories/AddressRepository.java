530f49d685424957d23679f5e449b041ee1d87a0
Thiago alves <thiagoubatuba123@gmail.com>
2024-07-01 09:48:00 -0300

    feat: add repositories

---
+++ api/src/main/java/com/tthiagoads/api/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.tthiagoads.api.repositories;
+
+import com.tthiagoads.api.domain.address.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
---
+++ api/src/main/java/com/tthiagoads/api/repositories/CouponRepository.java
@@ -0,0 +1,9 @@
+package com.tthiagoads.api.repositories;
+
+import com.tthiagoads.api.domain.coupon.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+}
