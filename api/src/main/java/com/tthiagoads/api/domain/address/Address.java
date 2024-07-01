267603a4f852025c5bb59e09ade2f0900a0c3485
Thiago alves <thiagoubatuba123@gmail.com>
2024-07-01 09:37:43 -0300

    create initial app

---
+++ api/src/main/java/com/tthiagoads/api/domain/address/Address.java
@@ -0,0 +1,23 @@
+package com.tthiagoads.api.domain.address;
+
+import com.tthiagoads.api.domain.event.Event;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name="event-id")
+    private Event event;
+
+}
